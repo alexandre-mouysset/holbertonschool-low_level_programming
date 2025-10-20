110e945286172ef92926b55efc8b21dd83599386
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-20 12:03:48 +0200

    Fix: syntax inside printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - with proper grammar, but the outcome is a piece of art,
+ * main -print:  with proper grammar, but the outcome is a piece of art,
  * Return: always 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,);
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
