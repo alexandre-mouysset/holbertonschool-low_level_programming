df78850041a3fe357de0c386ac03a7b24ff874be
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-20 12:34:59 +0200

    Fix: sizeof added

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,10 +5,11 @@
  */
 int main(void)
 {
-printf("Size of a char: 1 byte(s)\n");
-printf("Size of an int: 4 byte(s)\n");
-printf("Size of a long int: 4 byte(s)\n");
-printf("Size of a long long int: 8 byte(s)\n");
-printf("Size of a float: 4 byte(s)\n");
+printf("Size of char: %zu byte(s)\n", sizeof(char));
+printf("Size of int: %zu byte(s)\n", sizeof(int));
+printf("Size of short int: %zu byte(s)\n", sizeof(short int));
+printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of float: %zu byte(s)\n", sizeof(float))
 return (0);
 }
