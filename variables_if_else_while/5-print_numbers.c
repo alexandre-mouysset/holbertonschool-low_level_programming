5e7742b03e195031e9c82b707fcf3b271bdf8e8e
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-21 14:13:02 +0200

    Feat: program that print all the decimal

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print 0123456789
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int decimal;
+	for (decimal = 0; decimal < 10; decimal++)
+	{
+		printf("%d", decimal);
+	}
+	printf("\n");
+return (0);
+}
