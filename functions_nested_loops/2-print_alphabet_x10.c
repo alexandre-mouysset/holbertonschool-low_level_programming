03b37e9d2d9c5c48270ec0faf071e58491218cc7
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-22 13:46:28 +0200

    Feat: program that print alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+char alphabet;
+int i;
+
+for (i = 0; i < 10; i++){
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+}
+}
