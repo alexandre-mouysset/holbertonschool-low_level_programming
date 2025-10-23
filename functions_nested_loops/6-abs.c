3200556a4140ddea277e35c8ea9891e68b0f57e1
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-23 12:06:51 +0200

    Feat: script that return aboslute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _abs - return absolute value
+* @n: The character to check absolute
+* Return: return -n if negative else n
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
