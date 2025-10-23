1f0588863644fc3946e2ff9f792242c0a8bb15d2
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-23 15:28:53 +0200

    Feat : added a function that print the hour every minute

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* jack_bauer - print hours
+* @: Hour and minute
+* Return: always 0
+*/
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+			_putchar('\n');
+		}
+	}
+}
