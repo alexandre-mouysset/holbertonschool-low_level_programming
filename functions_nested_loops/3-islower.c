18925f29949bb19fd78360d705349593fe69d935
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-23 10:21:08 +0200

    Fix: added some commentary

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+ * _islower - return 1 if lowercase
+ * @c: The character to check (ASCII value)
+ * Return: Always 0 (Success)
+ */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
