0798f05adad62a6ab15e5a72c2d505d1f633f8a2
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-24 11:26:06 +0200

    Feat: function that print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - print table
+* @n: print to 98
+* Return: always 0
+*/
+
+void print_to_98(int n)
+{
+
+	for (; n < 99; n++)
+	{
+		printf("%d, ", n);
+	}
+	for (; n > 99; n--)
+	{
+		printf("%d, ", n);
+	}
+}
