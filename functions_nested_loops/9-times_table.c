a0a0aaa7061e5eca75dc0aa5fdd7fdd9115fa675
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-24 09:45:35 +0200

    Feat: wrote a function that prints the 9 times tables starting with 0

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* times_table - print table
+* @: Hour and minute
+* Return: always 0
+*/
+
+void times_table(void)
+{
+	int table, multiplicateur, resultat;
+
+
+	for (table = 0; table < 10; table++)
+	{
+		for (multiplicateur = 0; multiplicateur < 10; multiplicateur++)
+		{
+			resultat = table * multiplicateur;
+
+			if (resultat <= 9)
+			{
+				_putchar('0' + resultat);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('0' + (resultat / 10));
+				_putchar('0' + (resultat % 10));
+				_putchar(',');
+				_putchar(' ');
+			}
+
+		}
+	}
+
+}
