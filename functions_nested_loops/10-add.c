ae9cb0854ae6b9e422d8c8160664f6340d099ccd
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-24 10:53:41 +0200

    Feat: function that adds two integers and return the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* add - addition
+* @a : the calcul
+* @b : the calcul
+* Return: always 0
+*/
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
