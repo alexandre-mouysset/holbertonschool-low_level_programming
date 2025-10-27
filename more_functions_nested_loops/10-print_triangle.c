33176bbf48576f39fb3c642b22a6b80326504d73
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-27 14:04:34 +0100

    rollback last commit

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square using the '#' character
+ * @size: The size of the square (number of rows and columns)
+ *
+ * Description: This function prints a square of size `size`
+ * using the '#' character. Each line contains `size` '#'
+ * characters. If `size` is less than or equal to 0, the
+ * function simply prints a new line.
+ *
+ * Return: Nothing (void)
+ */
+
+void print_triangle(int size)
+{
+	
+}
