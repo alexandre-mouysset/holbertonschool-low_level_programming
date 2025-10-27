b4f22c5d18b65cab7ead82b2f1de1268e5e527c6
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-27 15:27:01 +0100

    Feat: print a triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,18 +1,38 @@
 #include "main.h"
 
 /**
- * print_square - Prints a square using the '#' character
- * @size: The size of the square (number of rows and columns)
+ * print_triangle - Prints a right-aligned triangle using the '#' character
+ * @size: The height and base length of the triangle
  *
- * Description: This function prints a square of size `size`
- * using the '#' character. Each line contains `size` '#'
- * characters. If `size` is less than or equal to 0, the
- * function simply prints a new line.
+ * Description: This function prints a right-aligned triangle made up of
+ * the '#' character. The triangle has `size` rows, and each row increases
+ * in the number of '#' characters from top to bottom. If `size` is less
+ * than or equal to 0, the function simply prints a new line.
  *
  * Return: Nothing (void)
  */
 
 void print_triangle(int size)
 {
-	
+	int l, c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (l = 1; l <= size; l++)
+	{
+		for (c = 1; c <= size; c++)
+		{
+			if (c <= (size - l))
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
 }
