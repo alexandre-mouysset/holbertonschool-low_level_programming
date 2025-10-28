f5c5892d7695f64e90f41f78a420aae09579fd10
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-28 09:49:19 +0100

    feat: function that returns the lenght of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: pointer to the string
+ *
+ *Return: the number of characters in the string, excluding the null terminator
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+return (i);
+}
