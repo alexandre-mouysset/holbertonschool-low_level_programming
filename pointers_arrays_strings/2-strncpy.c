6568e4daeee19651ed7d38cdfa8c8f62b4b5f652
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-30 10:45:53 +0100

    Fix: added new loop

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,15 +13,17 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i]; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		if (i < n)
 		{
 			dest[i] = src[i];
 		}
 	}
-
-	dest[i] = '\0';
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 
 return (dest);
 }
