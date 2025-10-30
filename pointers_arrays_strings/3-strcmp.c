3dc494bc517e631c55cc7cb0281352ee0cebac8d
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-30 11:31:49 +0100

    Feat function that compare 2 strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Compares s1 and s2 lexicographically.
+ *
+ * Return: 0 if equal, positive if s1 > s2, negative if s1 < s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+return (s1[i] - s2[i]);
+}
+
