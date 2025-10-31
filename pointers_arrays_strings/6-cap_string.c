239821f636b694aa92031409464c7b467ff8ebe0
alexandre-mouysset <12258@holbertonstudents.com>
2025-10-31 09:55:42 +0100

    fix: added a condition for the start of string and changed tabulation condition

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -15,12 +15,17 @@ char *cap_string(char *str)
 {
 	int i;
 
+	if (str[0] >= 97 && str[0] <= 122)
+		{
+		str[0] = (str[0] - 32);
+		}
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] == ',' || str[i] == ';' || str[i] == '.' ||
 			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
 			str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-			str[i] == '}' || str[i] == ' ' || str[i] == '	' ||
+			str[i] == '}' || str[i] == 32 || str[i] == '\t' ||
 			str[i] == '\n')
 		{
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
