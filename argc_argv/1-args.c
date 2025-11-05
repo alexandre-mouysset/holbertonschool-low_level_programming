7c721b7243f6cf6228afe3c12dacd0e43fe87f86
alexandre-mouysset <12258@holbertonstudents.com>
2025-11-05 10:54:26 +0100

    fix: added -1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -6,12 +6,12 @@
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: 0 on success, 1 if extra arguments are given
+ * Return: 0 on success
  */
 
 int main(int argc, char **argv)
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	(void)argv;
 	return (0);
 }
