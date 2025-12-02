1e169623d7194c6c002be5b6ed8dc8e4ed504c75
alexandre-mouysset <12258@holbertonstudents.com>
2025-12-02 11:41:57 +0100

    feat: function that adds a new node at the begginning of a list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_node - Adds a new node at the beginning of a linked list
+ * @head: Pointer to the pointer of the head of the list
+ * @str: String to store in the new node
+ *
+ * Return: Address of the new node, or NULL if it fails
+ *
+ * Description:
+ *   Allocates memory for a new node, duplicates the given string,
+ *   calculates its length, and inserts the node at the beginning of the list.
+ *   If memory allocation fails, returns NULL.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
